5901c051c32a094563bc09c4f2a466749ee9d690
Melissa Real <melissa.real@hotmail.com>
2018-08-14 14:04:38 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -3,7 +3,8 @@ require_relative '../lib/concerns/findable'
 require_relative '../lib/concerns/paramable'
 
 class Song
-
+  extend Memorable::ClassMethods
+  include Memorable::InstanceMethods
 
   extend Findable
   include Paramable
